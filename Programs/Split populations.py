2cc34e17861b6de36c08a6c12f7341bdd9f7b436
Deadline Was Yesterday <Deadlinewasyesterday@gmail.com>
2020-11-13 23:32:45 +0600

    coming together

--- Programs/Split populations.py
+++ Programs/Split populations.py
@@ -106,3 +106,105 @@ dfind = dfind.loc[keep, :]
 dfind.to_csv('WF2indR7.vcf', index = False, sep = '\t')
 
 
+
+
+dfaus = df.loc[:, ['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT',
+                  'IRIS_313-10587',
+ 'IRIS_313-10592',
+ 'IRIS_313-10593',
+ 'IRIS_313-10595',
+ 'IRIS_313-10598',
+ 'IRIS_313-10600',
+ 'IRIS_313-10602',
+ 'IRIS_313-10603',
+ 'IRIS_313-10604',
+ 'IRIS_313-10605',
+ 'IRIS_313-10606',
+ 'IRIS_313-10963',
+ 'IRIS_313-10964',
+ 'IRIS_313-10965',
+ 'IRIS_313-10976',
+ 'IRIS_313-10979',
+ 'IRIS_313-10981',
+ 'IRIS_313-10987',
+ 'IRIS_313-11013',
+ 'IRIS_313-11014',
+ 'IRIS_313-11015',
+ 'IRIS_313-11016',
+ 'IRIS_313-11017',
+ 'IRIS_313-11018',
+ 'IRIS_313-11019',
+ 'IRIS_313-11047',
+ 'IRIS_313-11048',
+ 'IRIS_313-11049',
+ 'IRIS_313-11050',
+ 'IRIS_313-11052',
+ 'IRIS_313-11053',
+ 'IRIS_313-11054',
+ 'IRIS_313-11056',
+ 'IRIS_313-11057',
+ 'IRIS_313-11058',
+ 'IRIS_313-11059',
+ 'IRIS_313-11060',
+ 'IRIS_313-11061',
+ 'IRIS_313-11063',
+ 'IRIS_313-11064',
+ 'IRIS_313-11065',
+ 'IRIS_313-11067',
+ 'IRIS_313-11111',
+ 'IRIS_313-11112',
+ 'IRIS_313-11116',
+ 'IRIS_313-11123',
+ 'IRIS_313-11124',
+ 'IRIS_313-11154',
+ 'IRIS_313-11163',
+ 'IRIS_313-11210',
+ 'IRIS_313-11213',
+ 'IRIS_313-11214',
+ 'IRIS_313-11216',
+ 'IRIS_313-11322',
+ 'IRIS_313-11323',
+ 'IRIS_313-11324',
+ 'IRIS_313-11481',
+ 'IRIS_313-11482',
+ 'IRIS_313-11483',
+ 'IRIS_313-11484',
+ 'IRIS_313-11557',
+ 'IRIS_313-12002',
+ 'IRIS_313-12055',
+ 'IRIS_313-12141',
+ 'IRIS_313-8252',
+ 'IRIS_313-8283',
+ 'IRIS_313-8321',
+ 'IRIS_313-8410',
+ 'IRIS_313-8641',
+ 'IRIS_313-8721',
+ 'IRIS_313-8789',
+ 'IRIS_313-8822',
+ 'IRIS_313-8864',
+ 'IRIS_313-8963',
+ 'IRIS_313-9368',
+ 'IRIS_313-9422',
+ 'IRIS_313-9626',
+ 'IRIS_313-9661']]
+
+
+
+#ref allele filter
+keep = []
+for i,r in dfaus.iterrows():
+    if ''.join(r).count('0/0') > 4:
+        keep.append(i)
+        
+dfaus = dfaus.loc[keep, :].reset_index(drop = True)
+
+dfaus.to_csv('WF2ausR4.vcf', index = False, sep = '\t')
+
+keep = []
+for i,r in dfaus.iterrows():
+    if ''.join(r).count('0/0') > 7:
+        keep.append(i)
+        
+dfaus = dfaus.loc[keep, :]
+
+dfaus.to_csv('WF2ausR7.vcf', index = False, sep = '\t')
